a55c262019a149d5a8b4b99f0e15bf4c81767565
Павел Йорданов Найденов <stu2001261002@uni-plovdiv.bg>
2021-11-11 15:11:40 +0200

    Homework 1

--- ProjectOne/ProjectOne/Program.cs
+++ ProjectOne/ProjectOne/Program.cs
@@ -10,7 +10,63 @@ namespace ProjectOne
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //We will enter the input which is text (Book).
+            string[] input = Console.ReadLine()
+                 .Split(new string[] { " ", ", ", ". ", "- ",
+                     "!", "?", "„", "“" }, StringSplitOptions.RemoveEmptyEntries);
+
+            GetCountOfWords(input);
+            GetShortestWord(input);
+            GetLongestWord(input);
+            GetAvarageLenghtOfWords(input);
+          
+           
+        }
+
+        private static void GetCountOfWords(string[] input)
+        {
+            int number = input.Length;
+            Console.WriteLine("There are total of " + number + " words!");
+        }
+
+        private static void GetShortestWord(string[] input)
+        {
+            int numberOfChars = int.MaxValue;
+            string shortestWord = "";
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i].Length < numberOfChars)
+                {
+                    numberOfChars = input[i].Length;
+                    shortestWord = input[i];
+                }
+            }
+
+            Console.WriteLine("The shortest word is - " + shortestWord);
+        }
+
+        private static void GetLongestWord(string[] input)
+        {
+            int numberOfChars = int.MinValue;
+            string longestWord = "";
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i].Length > numberOfChars)
+                {
+                    numberOfChars = input[i].Length;
+                    longestWord = input[i];
+                }
+            }
+
+            Console.WriteLine("The longest word is - " + longestWord);
+        }
+
+        private static void GetAvarageLenghtOfWords(string[] input)
+        {
+            double averageLength = input.Average(w => w.Length);
+            Console.WriteLine("The average length of all words is - " + averageLength);
         }
     }
 }
